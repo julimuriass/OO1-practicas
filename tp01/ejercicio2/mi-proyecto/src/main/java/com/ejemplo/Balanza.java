335eade344c429a51996ea278200d93868a52a2a
julimuriass <julieta.murias@gmail.com>
2025-09-11 21:28:40 -0300

    Modified ex02, and added new functionalities to it (ex04).

--- tp01/ejercicio2/mi-proyecto/src/main/java/com/ejemplo/Balanza.java
+++ tp01/ejercicio2/mi-proyecto/src/main/java/com/ejemplo/Balanza.java
@@ -1,17 +1,29 @@
 package com.ejemplo;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Balanza {
     Integer cantDeProductos = 0;
     double precioTotal;
     double pesoTotal;
+    ArrayList<Producto> productos = new ArrayList<>(); //Está bien inicializarla acá??????
+
+
+    //Comportamiento.
+
+    public List<Producto> getProductos() { 
+        return this.productos;
+    }
 
     //Pone en cero todos los valores de la balanza.
     void ponerEnCero() {
         this.cantDeProductos = 0;
         this.pesoTotal = 0;
         this.precioTotal = 0;
+
+        this.productos.clear();
     }
 
     //Agrega producto a la balanza (modificando cantidad de productos, el precio y el peso total).
@@ -19,6 +31,8 @@ public class Balanza {
         this.cantDeProductos += 1;
         this.precioTotal += producto.getPrecio();
         this.pesoTotal += producto.getPeso();
+
+        this.productos.add(producto);
     }
 
     Ticket emitirTicket() {
