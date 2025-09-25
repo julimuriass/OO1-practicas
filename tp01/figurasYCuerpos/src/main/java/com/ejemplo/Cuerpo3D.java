8072155b0a945c4d5b86e316a63e47aaf47322f5
julimuriass <julieta.murias@gmail.com>
2025-09-25 16:06:49 -0300

    Modified ex07. I have doubts.

--- tp01/figurasYCuerpos/src/main/java/com/ejemplo/Cuerpo3D.java
+++ tp01/figurasYCuerpos/src/main/java/com/ejemplo/Cuerpo3D.java
@@ -1,29 +1,36 @@
 package com.ejemplo;
 
-public class Cuerpo3D {
+public abstract class Cuerpo3D { 
     private Figuras2d cuerpo; //Cuerpo va a ser de cualquier clase que implemente esta interfaz.
     private double altura;
 
+    public Cuerpo3D() {
+        this.altura = 1;
+    }
 
     //Setters.
-    void setAltura(double altura) {
-        this.altura = altura;
-    }
+    public abstract void setAltura(double altura);
 
-    void setCaraBasal(Figuras2d figura) {
-        this.cuerpo = figura;
-    }
+    
+
+    public abstract void setCaraBasal(Figuras2d figura);
 
     //Getters.
 
    // El área o superficie exterior de un cuerpo es:  2* área-cara-basal + perímetro-cara-basal * altura-del-cuerpo
   //El volumen de un cuerpo es: área-cara-basal * altura
 
-    double getArea() {
+    double getSuperficieExterior() {
         return 2 * this.cuerpo.getArea() + this.cuerpo.getPerimetro() * this.altura;
     }
 
     double getVolumen() {
         return this.cuerpo.getArea() * this.altura;
     }
+
+    double getAltura() {
+        return this.altura;
+    }
+
+
 }
